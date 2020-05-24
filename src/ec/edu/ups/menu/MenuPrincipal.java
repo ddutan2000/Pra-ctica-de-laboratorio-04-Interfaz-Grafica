6a0b3c71d335b2fea63876842ce13d9af68e2279
Denys <adriandutan15@gmail.com>
2020-05-24 18:24:06 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/menu/MenuPrincipal.java
+++ src/ec/edu/ups/menu/MenuPrincipal.java
@@ -5,10 +5,41 @@
  */
 package ec.edu.ups.menu;
 
+import java.awt.*;
+
 /**
  *
  * @author Dutan2000
  */
 public class MenuPrincipal {
-    
+    public static void main(String []args){
+        //creamos marco
+        Frame marco=new Frame();
+        marco.setTitle("Menu");
+        marco.setSize(600,400);
+        
+        //creamos gridLayout
+        GridLayout layout=new GridLayout(3, 1);
+        
+        //creamos panel principal con parametros de gridLayout
+        Panel panelPrincipal=new Panel(layout);
+        
+        //creamos botones
+        Button boton1=new Button("Iniciar Sesion");
+        Button boton2=new Button("Registrarse");
+        Button boton3=new Button("Mas");
+        
+        //agregar botones a panel
+        panelPrincipal.add(boton1);
+        panelPrincipal.add(boton2);
+        panelPrincipal.add(boton3);
+        
+        //agregar panel a marco 
+        marco.add(panelPrincipal);
+        marco.setVisible(true);
+        
+        
+        
+                
+    }
 }
