bae96ea02013e8b70fe2872a3afbb32bd002d23b
Denys <adriandutan15@gmail.com>
2020-05-24 18:03:23 -0500

    inicializacion de proyecto

---
+++ src/ec/edu/ups/menu/Menu.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.menu;
+
+import java.awt.*;
+
+/**
+ *
+ * @author Dutan2000
+ */
+public class Menu {
+    public static void main(String []args){
+        //Frame  
+        Frame miventana=new Frame();
+        miventana.setTitle("Practica de laboratorio 04 ");
+        miventana.setSize(600,400);
+        
+       //borderlayout
+       GridLayout layout=new GridLayout(5, 1);
+       
+       //panel General con un borderlayout
+       Panel panelGeneral=new Panel(layout);
+        
+       //panel Secunadrio 
+       //Panel panelCentro=new Panel();
+       
+       //Flowlayout
+       //FlowLayout opcionesMenu=new FlowLayout();
+       
+       //declarar botones 
+       Button boton1=new Button("Registrar");
+       Button boton2=new Button("Borrar");
+       Button boton3 =new Button("Modificar");
+       Button boton4=new Button("Buscar telefono");
+       Button boton5=new Button("Listar Telefonos");
+       
+       //agrgar botones al panel secudario
+       panelGeneral.add(boton1);
+       panelGeneral.add(boton2);
+       panelGeneral.add(boton3);
+       panelGeneral.add(boton4);
+       panelGeneral.add(boton5);
+       
+       //agregar panel secundario al panel principal 
+      
+
+       
+       miventana.add(panelGeneral);
+       miventana.setVisible(true);
+       
+       
+       
+    }
+}
