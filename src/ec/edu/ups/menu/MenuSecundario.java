d8a4688ce70f4db32cbd6fe7bbe281ea20e7cf40
Denys <adriandutan15@gmail.com>
2020-05-24 22:21:48 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/menu/MenuSecundario.java
+++ src/ec/edu/ups/menu/MenuSecundario.java
@@ -5,10 +5,38 @@
  */
 package ec.edu.ups.menu;
 
+import java.awt.*;
+
 /**
  *
  * @author Dutan2000
  */
 public class MenuSecundario {
-    
+
+    public static void main(String[] args) {
+        //creamos marco
+        Frame marco = new Frame();
+        marco.setTitle("Menu Secundario");
+        marco.setSize(600, 400);
+
+        //creamos gridLayout
+        GridLayout layout = new GridLayout(3, 1);
+
+        //creamos panel principal con parametros de gridLayout
+        Panel panelPrincipal = new Panel(layout);
+
+        //creamos botones
+        Button boton1 = new Button("Telefono");
+        Button boton2 = new Button("Configuracion");
+        Button boton3 = new Button("Salir");
+
+        //anadimos los botones al panel 
+        panelPrincipal.add(boton1);
+        panelPrincipal.add(boton2);
+        panelPrincipal.add(boton3);
+
+        //agregar panel a marco 
+        marco.add(panelPrincipal);
+        marco.setVisible(true);
+    }
 }
