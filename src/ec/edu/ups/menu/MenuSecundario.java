c067f27acc3da7fe6c9c95d12dc711cae61ee463
Denys <adriandutan15@gmail.com>
2020-05-24 22:10:26 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/menu/MenuSecundario.java
+++ src/ec/edu/ups/menu/MenuSecundario.java
@@ -5,47 +5,10 @@
  */
 package ec.edu.ups.menu;
 
-import java.awt.*;
-
 /**
  *
  * @author Dutan2000
  */
 public class MenuSecundario {
-    public static void main(String []args){
-        //Frame  
-        Frame miventana=new Frame();
-        miventana.setTitle("Practica de laboratorio 04 ");
-        miventana.setSize(600,400);
-        
-       //borderlayout
-       GridLayout layout=new GridLayout(5, 1);
-       
-       //panel General con un borderlayout
-       Panel panelGeneral=new Panel(layout);
-        
-       //declarar botones 
-       Button boton1=new Button("Registrar");
-       Button boton2=new Button("Borrar");
-       Button boton3 =new Button("Modificar");
-       Button boton4=new Button("Buscar telefono");
-       Button boton5=new Button("Listar Telefonos");
-       
-       //agrgar botones al panel secudario
-       panelGeneral.add(boton1);
-       panelGeneral.add(boton2);
-       panelGeneral.add(boton3);
-       panelGeneral.add(boton4);
-       panelGeneral.add(boton5);
-       
-       //agregar panel secundario al panel principal 
-      
-
-       
-       miventana.add(panelGeneral);
-       miventana.setVisible(true);
-       
-       
-       
-    }
+    
 }
